9985b7057d87c2389d9d61922813bb49f1f2c501
rmachnik <rmachnik@yahoo.pl>
2018-07-12 21:49:22 +0200

    fixing bug

--- src/main/java/application/ApplicationConfig.java
+++ src/main/java/application/ApplicationConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("application.rest.controllers")
+@ComponentScan("application.rest")
 public class ApplicationConfig {
 
     final InMemoryUserRepository userRepository = new InMemoryUserRepository();
